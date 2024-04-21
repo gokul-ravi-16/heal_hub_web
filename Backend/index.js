70d909886e3b8b5f54270c14406c9e39a61aec5a
gokul-ravi-16 <154631237+gokul-ravi-16@users.noreply.github.com>
2024-04-21 16:51:35 -0400

    push

---
+++ Backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dbConnect = require('./Config/dbConnect');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const authRouter = require('./Routes/authRoute'); 
+const cors = require('cors');
+
+ 
+const app = express();
+app.use(cors(
+ {
+ 
+ }
+));
+const{notFound, errorHandler} = require('./Middlewares/errorHandler');
+app.use(express.static('public'));
+  // Initialize express app here
+
+// Load environment variables from .env file
+dotenv.config({ path: './Routes/.env' });  
+
+// Connect to the database
+dbConnect();
+
+//mongoose.connect('mongodb+srv://gokulravi221600:Healhub2024@cluster0.r20ki9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: false }));   
+const Port = process.env.PORT || 3000; 
+app.use('/api/user', authRouter); 
+
+app.use(notFound);
+app.use(errorHandler);
+app.listen(Port, () => { 
+    console.log(`Server is running on port ${Port}`);   
+});  
+
+   
